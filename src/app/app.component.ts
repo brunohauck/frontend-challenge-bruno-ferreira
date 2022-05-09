b940e7ed15f87e54b5efd66a6692c78c8b9cea8f
Bruno Hauck <brunohauck@Brunos-MacBook-Air.local>
2022-05-09 19:00:45 -0300

    Some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   public appPages = [
-    { title: 'Inbox', url: '/folder/Inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/Outbox', icon: 'paper-plane' },
+    { title: 'Home', url: '/home', icon: 'mail' },
+    { title: 'Logout', url: '/login', icon: 'paper-plane' },
     { title: 'Favorites', url: '/folder/Favorites', icon: 'heart' },
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
